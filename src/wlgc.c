731434dea759c9158c05e963e8a22ca5fcdb1b74
zinkt <zinkt@foxmail.com>
2023-03-19 15:42:33 +0800

    gc 完成 未测试

--- src/wlgc.c
+++ src/wlgc.c
@@ -13,15 +13,16 @@ void gc_init() {
     log_debug("gc init");
     // 将bos设置为调用函数的起始地址
     gc.bos = __builtin_frame_address(1);
+    gc.capacity = DEFAULT_CAPACITY;
+    gc.allocs = calloc(gc.capacity, sizeof(alloc_t *));
     gc.size = 0;
-    gc.sweep_limit = (int)(DEFAULT_CAPACITY * SWEEP_FACTOR);
-    
+    gc.sweep_limit = (int)(gc.capacity * SWEEP_FACTOR);
 }
 
 static char gc_need_collect() {return gc.size > gc.sweep_limit;}
 
 static alloc_t *search(void *p) {
-    log_debug("search alloc: %p", p);
+    log_debug("Search alloc: %p", p);
     // 在DEFAULT_CAPACITY为2的整次幂时，使用& (DEFAULT_CAPACITY-1)这种取模方式更快
     alloc_t *ret = gc.allocs[hash(p) & (DEFAULT_CAPACITY-1)];
     while (ret)
@@ -36,7 +37,7 @@ static alloc_t *search(void *p) {
 
 
 static void gc_mark_alloc(alloc_t *alloc) {
-    log_debug("mark alloc(%p) and it's children", alloc);
+    log_debug("Mark alloc(%p) and it's children", alloc);
     // 如果已标记，则跳过
     if(alloc && !(alloc->size_tag & 0x1)) {
         // 标记
@@ -51,7 +52,7 @@ static void gc_mark_alloc(alloc_t *alloc) {
 }
 
 static void gc_mark() {
-    log_debug("start mark phase");
+    log_debug("Start mark phase");
     // 扫描栈，获取根节点，并直接从根节点开始递归标记
     jmp_buf jb;
     setjmp(jb);
@@ -66,10 +67,11 @@ static void gc_mark() {
 }
 
 static void gc_free_alloc(alloc_t *alloc) {
+    log_debug("Free an alloc");
     if(!alloc) return;
 
     // 将其从哈希表中删去
-    int index = hash(alloc->p) & (DEFAULT_CAPACITY-1);
+    uint32_t index = hash(alloc->p) & (DEFAULT_CAPACITY-1);
     alloc_t *p = gc.allocs[index], *q = NULL;
     // 如果是第一个
     if(p == alloc) {
@@ -91,10 +93,31 @@ static void gc_free_alloc(alloc_t *alloc) {
     --gc.size;
 }
 
+// 扩容hash表
+static void gc_hash_resize() {
+    log_debug("Resizing hash: double capacity");
+    size_t new_cap = gc.capacity * 2;
+    alloc_t **new_allocs = calloc(new_cap, sizeof(alloc_t *));
+    for(size_t i = 0; i < gc.capacity; ++i) {
+        alloc_t *alloc = gc.allocs[i];
+        while(alloc) {
+            alloc_t *next_alloc = alloc->next;
+            uint32_t index = hash(alloc->p) & (new_cap-1);
+            alloc->next = new_allocs[index];
+            new_allocs[index] = alloc;
+            alloc = next_alloc;
+        }
+    }
+    free(gc.allocs);
+    gc.capacity = new_cap;
+    gc.allocs = new_allocs;
+    gc.sweep_limit = gc.capacity * SWEEP_FACTOR;
+}
+
 void gc_sweep() {
     if(!gc.size) return;
     // 遍历哈希表
-    for(int i = 0; i < DEFAULT_CAPACITY; i++) {
+    for(size_t i = 0; i < DEFAULT_CAPACITY; i++) {
         alloc_t *alloc = gc.allocs[i];
         alloc_t *next = NULL;
         while(alloc) {
@@ -108,6 +131,11 @@ void gc_sweep() {
                 alloc = next;
             }
         }
+    }
+    // 如果回收后仍超过SWEEP_FACTOR
+    // 则触发hash_resize
+    if(gc_need_collect()) {
+        gc_hash_resize();
     }
 }
 
@@ -128,9 +156,8 @@ void *gc_malloc(size_t size) {
         alloc->p = ret;
         alloc->size_tag = sz;
         alloc->next = NULL;
-        alloc->destructor = NULL;
         // 插入到hash表中
-        int index = hash(ret)&(DEFAULT_CAPACITY-1);
+        uint32_t index = hash(ret)&(DEFAULT_CAPACITY-1);
         alloc_t * a = gc.allocs[index];
         gc.allocs[index] = alloc;
         alloc->next = a;
--- src/wlgc.h
+++ src/wlgc.h
@@ -6,9 +6,8 @@
 
 #define MIN_CAPACITY        1024    // 最小哈希表大小
 #define DEFAULT_CAPACITY    1024    // 默认初始hash表大小
-#define DOWNSIZE_FACTOR     0.2     // 高水位
-#define UPSIZE_FACTOR       0.8     // 低水位
-#define SWEEP_FACTOR        0.5     // 清除水位
+#define LOAD_FACTOR         0.7     // hash表扩容比例
+#define SWEEP_FACTOR        0.5     // 触发清除
 
 
 // static uint8_t *__rbp;
@@ -26,7 +25,6 @@ struct alloc_s
 {
     void *p;                    // 指向分配内存的指针
     size_t size_tag;            // 该段空间的大小，以及标记-清除的tag
-    void (*destructor)(void*);  // destructor func
     alloc_t *next;              // 链
 };
 
@@ -35,13 +33,14 @@ struct gc_s
     void *bos;                  // bottom of stack 栈底
 
     // 记录已分配的内存
-    alloc_t* allocs[DEFAULT_CAPACITY];  // 已分配 哈希表
+    alloc_t** allocs;  // 已分配 哈希表
+    size_t capacity;                    // allocs容量
     size_t size;                        // 已分配alloc数
     size_t sweep_limit;
 };
 
 // Jenkin's 32 bit hash function (Guarantee's a good hash)
-int hash(void *key) {
+uint32_t hash(void *key) {
 	long a = (long)key;
 	a = (a + 0x7ed55d16) + (a << 12);
 	a = (a ^ 0xc761c23c) ^ (a >> 19);
@@ -49,7 +48,7 @@ int hash(void *key) {
 	a = (a + 0xd3a2646c) ^ (a << 9);
 	a = (a + 0xfd7046c5) + (a << 3);
 	a = (a ^ 0xb55a4f09) ^ (a >> 16);
-	return (int)a;
+	return (uint32_t)a;
 }
 
 void gc_init();
